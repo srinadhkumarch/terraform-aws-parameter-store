821e00c91c2a4122dbce93a7c2a376b60758b08c
Srinadh Kumar Chalamalasetti <71437478+srinadhkumarch@users.noreply.github.com>
2021-08-03 22:31:15 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,18 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = ">= 3.43.0"
     }
   }
 
-  required_version = ">= 1.0.0"
-}
-
-provider "aws" {
-  region = "eu-west-2"
-}
-
-provider "aws" {
-  alias  = "Ireland"
-  region = "eu-west-1"
 }
